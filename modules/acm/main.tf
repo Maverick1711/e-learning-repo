ed9ff9bf6bdea7d229c208178dc4b7b3ba13d3cc
Maverick1711 <olanipekunoladotun@gmail.com>
2023-04-29 16:23:50 +0100

    Update ACM

--- modules/acm/main.tf
+++ modules/acm/main.tf
@@ -1,7 +1,6 @@
 # request public certificates from the amazon certificate manager.
 resource "aws_acm_certificate" "elearning_acm_cert" {
   domain_name       = "${var.domain_name}"
-  subject_alternative_names = ["*.${var.domain_name}"]
   validation_method = "DNS"
 
   lifecycle {
