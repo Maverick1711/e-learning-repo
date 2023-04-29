42a84523583dfebd78dfff6544c05bd094d21fb6
Maverick1711 <olanipekunoladotun@gmail.com>
2023-04-29 13:01:53 +0100

    ACM module updated

--- modules/acm/main.tf
+++ modules/acm/main.tf
@@ -11,14 +11,21 @@ resource "aws_acm_certificate" "elearning_acm_cert" {
 
 
 # create a record set in route 53 for domain validatation
-resource "aws_route53_record" "elearning_cert_dns" {
-  allow_overwrite = true
-  name =  tolist(aws_acm_certificate.elearning_acm_cert.domain_validation_options)[0].resource_record_name
-  records = [tolist(aws_acm_certificate.elearning_acm_cert.domain_validation_options)[0].resource_record_value]
-  type = tolist(aws_acm_certificate.elearning_acm_cert.domain_validation_options)[0].resource_record_type
-  zone_id = var.aws_route53_zone_id
+resource "aws_route53_record" "route_53_record" {
+  for_each = {
+    for dvo in aws_acm_certificate.elearning_acm_cert.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
 
-  ttl = 60
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = var.aws_route53_zone_id
 }
 
 resource "aws_acm_certificate_validation" "elearning_cert_validate" {
